c1efa1247ba819e396bcb7211a2e7267f413de65
bone <coder.bone@gmail.com>
2017-06-10 13:35:32 +1000

    loadConfig doesn't work because it's passing defaultConfigPath to _loadConfig instead of filePath

--- nodz_main.py
+++ nodz_main.py
@@ -7,7 +7,7 @@ import nodz_utils as utils
 
 
 
-defautConfigPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'default_config.json')
+defaultConfigPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'default_config.json')
 config = None
 
 
@@ -426,7 +426,7 @@ class Nodz(QtWidgets.QGraphicsView):
     # API
     ##################################################################
 
-    def loadConfig(self, filePath=defautConfigPath):
+    def loadConfig(self, filePath=defaultConfigPath):
         """
         Set a specific configuration for this instance of Nodz.
 
@@ -435,7 +435,7 @@ class Nodz(QtWidgets.QGraphicsView):
                          use.
 
         """
-        data = utils._loadConfig(filePath=defautConfigPath)
+        data = utils._loadConfig(filePath)
 
         global config
         config = data
