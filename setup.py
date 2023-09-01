23f1abfeb3425e752a1fe2d27a9788a26b9bba25
Hasiel Alvarez <hasielhassan@gmail.com>
2023-09-01 01:39:21 -0700

    Adjustments on setup

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ import sys
 path = os.path.dirname(__file__)
 sys.path.insert(0, path)
 
-import Nodz
-
 from setuptools import setup, find_packages
 
-version = "1.3.0"
-
+# patched version from original
+version = "1.3.0-1"
 
 classifiers = [
     "Development Status :: 3 - Alpha",
@@ -20,6 +18,7 @@ classifiers = [
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.7",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Utilities"
 ]
@@ -28,11 +27,12 @@ classifiers = [
 setup(
     name="Nodz",
     version=version,
-    description="Python 2 compatible library to create nodes based graphs with Qt- "
+    description="Python 2 and 3 compatible library to create nodes based graphs with Qt- "
                 "It can work with any of PySide, PySide2, PyQt4 and PyQt5 thanks to the Qt.py module.",
     author="Loic LeGoff",
     author_email="loic.legoff9@gmail.com",
-    url="https://github.com/LeGoffLoic/Nodz",
+    # this is a patched version
+    url="https://github.com/hasielhassan/Nodz",
     license="MIT",
     zip_safe=False,
     package_data={'Nodz': ['default_config.json', 'pixmap.png']},
